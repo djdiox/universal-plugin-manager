740694a440ba35258998f37ad8decc153d393d93
Markus <markus.wagner@zoi.de>
2022-06-06 03:19:46 +0200

    more adjustments

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import {exec} from 'child_process';
-
+import config from '../public/config.json';
 function execute(command: string) : Promise<string> {
     return new Promise((resolve, reject) => {
         exec(command, function (error, stdout, stderr) {
@@ -14,10 +14,14 @@ function execute(command: string) : Promise<string> {
 };
 
 export default async () => {
-    const config = JSON.parse(fs.readFileSync('../public/config.json', 'utf-8'));
-    const passwordApp = config.apps.find((e: any) => e.name.toLowerCase() === '1password');
-    let result = await execute(passwordApp.cli.commands['list-items']);
+    // const config = JSON.parse(fs.readFileSync('../public/config.json', 'utf-8'));
+    const passwordApp = config.apps.find((e: any) => e.name.toLowerCase() === '1password')
+    passwordApp?.cli
+    let result = await execute(passwordApp?.cli?.commands['list-items'] || '');
     fs.writeFileSync('../data/1password/items.json', result);
     console.log(JSON.parse(result));
+    config.packageManagers.forEach(packageManager => {
+        packageManager.cmd.
+    });
     //   const result = Buffer.from(buffer)
 };
