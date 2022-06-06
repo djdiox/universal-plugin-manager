b7c018f5d1c205ca02c712091ea62b51ce5b4d93
Markus <djdiox@yahoo.com>
2022-06-06 21:47:08 +0200

    added loading

--- backend/src/common/logger.ts
+++ backend/src/common/logger.ts
@@ -4,14 +4,17 @@ export class CustomLogger {
   // private logger: log4js.Logger;
   public static Loglevel = log4js.levels;
 
-  private static logger: log4js.Logger;
+  public static Logger: log4js.Logger;
 
- public static get Logger() { return this.logger; }
-  constructor() {
+
+  // public static get Logger() { return this.logger; }
+  constructor(className: string) {
+    CustomLogger.Logger = new log4js.Logger(className);
+    // logger = new log4js.Logger(className)
     CustomLogger.Loglevel = Object.values(log4js.levels).reduce((acc, current, index) => {
       acc[current] = log4js.levels.getLevel(current);
       return acc;
-    }, {})
+    }, {});
   }
 
   static init(currentContext: string): log4js.Logger {
@@ -21,13 +24,10 @@ export class CustomLogger {
     // }
     const json = JSON.parse(fs.readFileSync('../log4js.json', 'utf-8'));
     console.log('Init log4js', json)
-    CustomLogger.logger = log4js.getLogger(currentContext);
-    CustomLogger.logger.level = CustomLogger.Loglevel.getLevel('' + process.env.LOG_LEVEL)
-    CustomLogger.logger.debug("Logger has been initialized with level: ", process.env.LOG_LEVEL);
-    return CustomLogger.logger;
+    CustomLogger.Logger = log4js.getLogger(currentContext);
+    CustomLogger.Logger.level = CustomLogger.Loglevel.getLevel('' + process.env.LOG_LEVEL)
+    CustomLogger.Logger.debug("Logger has been initialized with level: ", process.env.LOG_LEVEL);
+    return CustomLogger.Logger;
   }
 
-  // log(level = log4js.levels.DEBUG, message: string, ...args: any[]): void {
-  //   logger.log(level, args, message, ...args)
-  // }
 }
--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import { exec } from 'child_process';
-import config from '../public/config.json';
 import { CustomLogger } from './common/logger';
-import axios from 'axios';
+let config = JSON.parse(fs.readFileSync('../public/config.json', 'utf8'))
+
+// import axios from 'axios';
 function execute(command: string): Promise<string> {
     return new Promise((resolve, reject) => {
         exec(command, function (error, stdout, stderr) {
@@ -14,25 +15,28 @@ function execute(command: string): Promise<string> {
         });
     })
 };
-
 export default async () => {
     var logger = CustomLogger.Logger;
-    CustomLogger.Logger.info('Starting Seed Actions', config);
-    CustomLogger.Logger.info('info', config);
+    logger.info('Starting Seed Actions', config);
+    logger.info('info', config);
     // const config = JSON.parse(fs.readFileSync('../public/config.json', 'utf-8'));
 
     const passwordApp = config.apps.find((e: any) => e.name.toLowerCase() === '1password')
     let result = await execute(passwordApp?.cmds?.json['list-items'] || '');
-    fs.writeFileSync('../data/1password/items.json', result);
-    const getItem = passwordApp?.cmds?.json['get-item'];
-    execute('get-item', getItem)
-    // const allItems = await axios.get(url, {
-        
-    // })
-    console.log(allItems);
-    config.packageManagers.map(packageManager => {
-        logger.info(`${packageManager.name} ${packageManager?.cmds?.json?.info`)
-    });
+    result = JSON.parse(result);
+    CustomLogger.Logger.info('Who gets smashed at kwicky mart?', passwordApp)
+    fs.writeFileSync('../data/1password/items.json', JSON.stringify(result, null, 2));
+    const getItem = passwordApp?.cmds?.json['get-item'].replace('{id]', result);
+    try {
+        const cmd = await execute(getItem || '');
+        logger.info(cmd + ' has been successfully tested');
+    } catch (error) {
+        logger.error(getItem, result, passwordApp);
+    }
 
-    //   const result = Buffer.from(buffer)
+    // console.log(allItems);
+    const packageManagers = config.packageManagers.map((packageManager: { name: any; cmds: { json: { info: any; }; }; }) => {
+        logger.info(`${packageManager.name} ${packageManager?.cmds?.json?.info}`);
+        return packageManager;
+    });
 };
