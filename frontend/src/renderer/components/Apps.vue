19a433a8e3cdfa8bd5408fba4222acfe7dec4315
Markus <djdiox@yahoo.com>
2022-06-10 16:30:36 +0200

    small fixes

--- frontend/src/renderer/components/Apps.vue
+++ frontend/src/renderer/components/Apps.vue
@@ -3,15 +3,17 @@
 </template>
 
 <script>
+import Keychain from '1password'
 export default {
-  mounted() {
-    Keychain = require("1password");
-    keychain = new Keychain();
-    keychain.load("./1password.cloudkeychain", function (err) {
-      console.log("Keychain has loaded");
-    });
-  },
-};
+  mounted () {
+    console.log(this.$electron)
+    // Keychain = require("1password");
+    keychain = new Keychain()
+    keychain.load('./1password.cloudkeychain', function (err) {
+      console.log('Keychain has loaded')
+    })
+  }
+}
 </script>
 
 <style>
