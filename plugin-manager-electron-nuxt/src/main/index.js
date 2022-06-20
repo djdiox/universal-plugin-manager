9b4fc9cebb97db91960a50eefd264db979117571
Markus <markus.wagner@zoi.de>
2022-06-20 02:51:42 +0200

    fixed build

--- plugin-manager-electron-nuxt/src/main/index.js
+++ plugin-manager-electron-nuxt/src/main/index.js
@@ -2,11 +2,13 @@ const { app } = require('electron')
 let myWindow = null
 
 const gotTheLock = app.requestSingleInstanceLock()
-
+// If we have any open windows, especially on debug
 if (!gotTheLock) {
+// we gonna quit the app
   app.quit()
 } else {
   app.on('second-instance', (event, commandLine, workingDirectory) => {
+    console.log('detected second instance', event, commandLine, workingDirectory)
     // Someone tried to run a second instance, we should focus our window.
     if (myWindow) {
       if (myWindow.isMinimized()) myWindow.restore()
@@ -16,7 +18,7 @@ if (!gotTheLock) {
 
   // Create myWindow, load the rest of the app, etc...
   app.on('ready', () => {
-    myWindow = require('./mainWindow').default;
+    myWindow = require('./mainWindow').default
   })
   app.on('window-all-closed', function () {
     // On macOS it is common for applications and their menu bar
